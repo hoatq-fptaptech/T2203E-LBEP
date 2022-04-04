dc60abbbd4d4e73daca4878b2afba3f87336263d
quanghoa <quanghoa@gmail.com>
2022-04-04 14:17:47 +0700

    Session 2 Demo

---
+++ Session2/Demo1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	int x = 10;
+	float pi = 3.14159;
+	double k = 5.12;
+	char m = '$';
+	bool t = true;
+	printf("pi = %f",pi);
+	printf("k = %lf",k);
+	if(x >5){
+		
+	}else{
+		
+	}
+	
+	if(t){
+		
+	}
+}
---
+++ Session2/Demo2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	char h = 'a';
+	h += 12;
+	printf("h = %c\n",h);
+	printf("number\" = %d\n",h);
+	int x = 5;
+	float y ;
+	y = x / ((float)2);
+	float k = sqrt(y);
+	printf("y = %f",y);
+	printf("k = %f",k);
+}
